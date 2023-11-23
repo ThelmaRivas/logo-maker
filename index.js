13f5f598b1fb4286871f66b4606d61ce30ded5a4
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-23 12:14:38 -0600

    Prompt questions
    Added the questions that the inquirer.prompt is going to ask the user

--- index.js
+++ index.js
@@ -4,3 +4,32 @@ const inquirer = require('inquirer');
 const path = require('path');
 const { Shape, Triangle, Circle, Square } = require('./lib/shapes');
 
+async function generateLogo() {
+    const { text, textColor, shape, shapeColor } = await inquirer.prompt([
+        {
+          type: 'input',
+          name: 'text',
+          message: 'Enter up to three characters:',
+          validate: (input) => {
+            return input.length <= 3;
+          },
+        },
+        {
+          type: 'input',
+          name: 'textColor',
+          message: 'Enter the text color:',
+        },
+        {
+          type: 'list',
+          name: 'shape',
+          message: 'Select a shape:',
+          choices: ['Circle', 'Triangle', 'Square'],
+        },
+        {
+          type: 'input',
+          name: 'shapeColor',
+          message: 'Enter the shape color:',
+        },
+      ]);
+   
+}
