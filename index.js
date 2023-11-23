3789705b64930e60f6da3604353cec7749a8afa8
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-23 12:06:33 -0600

    Import libraries
    Import fs, inquirer and path libraries, also import functions from another file js that we are going to define later

---
+++ index.js
@@ -0,0 +1,6 @@
+// Runs the application using imports from lib/
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const { Shape, Triangle, Circle, Square } = require('./lib/shapes');
+
