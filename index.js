c29d168d8a0ff7683421e6397dead2b70c53ffc4
ThelmaRivas <thelma.rivas00@gmail.com>
2023-11-23 12:16:21 -0600

    Switch statement
    Use switch statement to create an object based on the value of the shape variable

--- index.js
+++ index.js
@@ -7,29 +7,44 @@ const { Shape, Triangle, Circle, Square } = require('./lib/shapes');
 async function generateLogo() {
     const { text, textColor, shape, shapeColor } = await inquirer.prompt([
         {
-          type: 'input',
-          name: 'text',
-          message: 'Enter up to three characters:',
-          validate: (input) => {
-            return input.length <= 3;
-          },
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters:',
+            validate: (input) => {
+                return input.length <= 3;
+            },
         },
         {
-          type: 'input',
-          name: 'textColor',
-          message: 'Enter the text color:',
+            type: 'input',
+            name: 'textColor',
+            message: 'Enter the text color:',
         },
         {
-          type: 'list',
-          name: 'shape',
-          message: 'Select a shape:',
-          choices: ['Circle', 'Triangle', 'Square'],
+            type: 'list',
+            name: 'shape',
+            message: 'Select a shape:',
+            choices: ['Circle', 'Triangle', 'Square'],
         },
         {
-          type: 'input',
-          name: 'shapeColor',
-          message: 'Enter the shape color:',
+            type: 'input',
+            name: 'shapeColor',
+            message: 'Enter the shape color:',
         },
-      ]);
-   
-}
+    ]);
+
+    let shapeObj;
+    switch (shape) {
+        case 'Triangle':
+            shapeObj = new Triangle(shapeColor);
+            break;
+        case 'Circle':
+            shapeObj = new Circle(shapeColor);
+            break;
+        case 'Square':
+            shapeObj = new Square(shapeColor);
+            break;
+        default:
+            throw new Error('Invalid shape');
+    }
+
+};
